08ebe0df692a9259def6abc4a8b4751a389f1dd6
Masiu <ciszekmarcell@gmail.com>
2020-07-17 13:29:48 +0200

    testing hooks

---
+++ app/test/hooks/useCount.test.ts
@@ -0,0 +1,15 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useCount from "../../hooks/useCount";
+
+describe("Name of the group", () => {
+  test("should increment counter", () => {
+    const { result } = renderHook(() => useCount());
+
+    act(() => {
+      result.current.increment();
+    });
+
+    expect(result.current.count).toBe(1);
+    expect(typeof result.current.increment).toBe("function");
+  });
+});
